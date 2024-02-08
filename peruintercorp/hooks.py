163d46c84cc2fadb2cea3ef45f4b20e3b2110f7d
Alejandro <aaguirrek@ucsm.edu.pe>
2024-02-08 17:37:38 -0500

    dsa

--- peruintercorp/hooks.py
+++ peruintercorp/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/peruintercorp/css/peruintercorp.css"
+app_include_css = "/assets/peruintercorp/css/peruintercorp.css"
 # app_include_js = "/assets/peruintercorp/js/peruintercorp.js"
 
 # include js, css files in header of web template
